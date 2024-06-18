e29031c29ef6892c40217d3ec45c19986caa85b5
GuilhermeLCK <guilhermelimacosta@gmail.com>
2024-06-17 23:59:51 -0300

    Feito o atalho no input, ++ e --

--- src/Pages/Home/Index.jsx
+++ src/Pages/Home/Index.jsx
@@ -6,12 +6,20 @@ import Kds from "../../Components/Kds/Kds";
 import { FaAnglesRight } from "react-icons/fa6";
 
 function Index() {
+  const situacoes = [
+    "Pendentes",
+    "Em produção",
+    "Finalizados",
+    "Ultimos pedidos",
+  ];
   const [pedidos, setPedidos] = useState(data);
+  const [inputValue, setInputValue] = useState("");
 
   const moverPedido = (Codigo, novoStatus) => {
     setPedidos(
       pedidos.map((pedido) => {
         if (pedido.Codigo === Codigo) {
+          console.log(pedido);
           return { ...pedido, SituacaoPedido: novoStatus };
         }
         return pedido;
@@ -19,12 +27,37 @@ function Index() {
     );
   };
 
-  const situacoes = [
-    "Pendentes",
-    "Em produção",
-    "Finalizados",
-    "Ultimos pedidos",
-  ];
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      var valueDigitado = e.target.value;
+      if (valueDigitado.includes("++")) {
+        const code = valueDigitado.replace("++", "");
+        const matchedPedidos = pedidos.filter((p) => p.Identificador == code);
+
+        console.log(matchedPedidos);
+        if (matchedPedidos[0].SituacaoPedido == 4) {
+          return alert("Movimento ja encerrado");
+        }
+
+        moverPedido(
+          matchedPedidos[0].Codigo,
+          matchedPedidos[0].SituacaoPedido + 1
+        );
+        //  setInputValue("");
+      } else {
+        const code = valueDigitado.replace("--", "");
+        const matchedPedidos = pedidos.filter((p) => p.Identificador == code);
+        if (matchedPedidos[0].SituacaoPedido == 1) {
+          return alert("Movimento não pode ficar negativo");
+        }
+
+        moverPedido(
+          matchedPedidos[0].Codigo,
+          matchedPedidos[0].SituacaoPedido - 1
+        );
+      }
+    }
+  };
   return (
     <>
       <div className="container-iTake-home">
@@ -36,6 +69,16 @@ function Index() {
             <div className="container-iTake-header-informacoes-text">
               <h3>iTake</h3>
             </div>
+          </div>
+          <div>
+            <input
+              type="text"
+              value={inputValue}
+              onKeyPress={handleKeyPress}
+              onChange={(e) => {
+                setInputValue(e.target.value);
+              }}
+            />
           </div>
           <div className="container-iTake-header-logout">
             <h3>Sair</h3>
